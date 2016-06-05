6c1b9ca3c359353f6e9a8bab12c9de7e2e8366b6
Pin-Shiuan She <snakeneedy@gmail.com>
2016-06-05 17:48:07 +0800

    Use 'notes/views.py' to output HTML

--- djangoproject/notes/views.py
+++ djangoproject/notes/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 
 def hello_world (request):
-    return HttpResponse("Hello World!")
+    htmlcode = """<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <p>Hello World! {current_time}</p>
+</body>
+</html>
+    """.format(current_time=datetime.now())
+    return HttpResponse(htmlcode)
 
