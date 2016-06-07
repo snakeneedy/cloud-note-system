d8f1100588159aaef024ec37e49a71934c7144c2
Snakeneedy <snakeneedy@gmail.com>
2016-06-07 23:56:29 +0800

    New models in notes/

--- djangoproject/notes/models.py
+++ djangoproject/notes/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Post (models.Model):
+    title = models.CharField(max_length = 100)
+    content = models.TextField(blank = True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    modified_at = models.DateTimeField(auto_now = True)
+
