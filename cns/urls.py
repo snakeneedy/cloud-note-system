d5bfb34e71f3259bac979c3d054e6da337fdd31c
Snakeneedy <snakeneedy@gmail.com>
2016-06-12 00:13:23 +0800

    New home and note template

--- cns/urls.py
+++ cns/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from notes.views import home, note
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', home),
+    url(r'^note/', note),
 ]
--- notes/models.py
+++ notes/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+def Note():
+    title = ''
+    content = ''
+    tags = []
+    created_at = ''
+    modified_at = ''
+
